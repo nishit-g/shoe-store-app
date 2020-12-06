949925c7316ded84183692cff2c4193d82d2bbce
nishit-g <nishit.gupta6708@gmail.com>
2020-12-06 21:14:19 +0530

    viewModel logic

--- app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoelisting/ShoeListingFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoeListingBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,8 +20,10 @@ class ShoeListingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragmen
-        return inflater.inflate(R.layout.fragment_shoe_listing, container, false)
+        // Inflate the layout for this fragment
+        val binding : FragmentShoeListingBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_listing,container,false)
+
+        return binding.root
     }
 
 }
